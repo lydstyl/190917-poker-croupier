abc08858beed76b95d76f87ba5a479515fccc5ac
Gabriel Brun <lydstyl@gmail.com>
2019-09-17 14:17:40 +0200

    First structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Poker Croupier</h1>
+      <div className='board'>
+        <div className='card three_clubs'></div>
+        <div className='card ace_hearts'></div>
+        <div className='card king_diamonds'></div>
+        <div className='card five_hearts'></div>
+        <div className='card jack_diamonds'></div>
+      </div>
+      <button className='croupier-order'>
+        <h2>Distribute - Flop - Turn - River</h2>
+      </button>
+      <div className='players'>
+        <button className='player'>p1</button>
+        <button className='player'>p2</button>
+        <button className='player'>p3</button>
+        <button className='player'>p4</button>
+        <button className='player'>p5</button>
+        <button className='player'>p6</button>
+        <button className='player'>p7</button>
+        <button className='player'>p8</button>
+        <button className='player'>p9</button>
+        <button className='player'>+</button>
+      </div>
     </div>
   );
 }
